f1d43409a8875312bbfda7a9c47418907b0df907
Chuck Waltz <chuckwaltz@gmail.com>
2020-01-05 11:46:04 -0500

    Setup for Netlify deploy

--- src/config/fire.jsx
+++ src/config/fire.jsx
@@ -2,12 +2,12 @@ import firebase from "firebase";
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyD7PL9JoSZT6e0cHHurZpM66jOtzVWlIvs",
-  authDomain: "artists-choice.firebaseapp.com",
-  databaseURL: "https://artists-choice.firebaseio.com",
-  projectId: "artists-choice",
-  storageBucket: "artists-choice.appspot.com",
-  messagingSenderId: "1056341225820"
+  apiKey: process.env.REACT_APP_ARTISTS_CHOICE_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_ARTISTS_CHOICE_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_ARTISTS_CHOICE_FIREBASE_DATABASEURL,
+  projectId: process.env.REACT_APP_ARTISTS_CHOICE_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_ARTISTS_CHOICE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_ARTISTS_CHOICE_CLIENTID
 };
 
 const fire = firebase.initializeApp(config);
